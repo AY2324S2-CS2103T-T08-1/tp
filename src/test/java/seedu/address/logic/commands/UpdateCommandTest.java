2b77625ae0d26a71eb70cc5cd57930d8949c784d
Alexander Setyawan <alexgenrong@gmail.com>
2024-03-28 23:28:01 +0800

    2nd attempt to fix UpdateCommandTest.java

--- src/test/java/seedu/address/logic/commands/UpdateCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UpdateCommandTest.java
@@ -1,2 +1,203 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATEOFBIRTH_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_SEX_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_STATUS_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.UpdateCommand.UpdatePersonDescriptor;
+import seedu.address.model.ImmuniMate;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Nric;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
+import seedu.address.testutil.UpdatePersonDescriptorBuilder;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for EditCommand.
+ */
+public class UpdateCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_allFieldsSpecifiedUnfilteredList_success() {
+        Person editedPerson = new PersonBuilder().build();
+        UpdatePersonDescriptor descriptor = new UpdatePersonDescriptorBuilder(editedPerson).build();
+        UpdateCommand updateCommand = new UpdateCommand(descriptor.getNric(), descriptor);
+
+        String expectedMessage = String.format(
+                UpdateCommand.MESSAGE_UPDATE_PERSON_SUCCESS, Messages.format(editedPerson));
+
+        Model expectedModel = new ModelManager(new ImmuniMate(model.getImmuniMate()), new UserPrefs());
+        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
+
+        assertCommandSuccess(updateCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_someFieldsSpecifiedUnfilteredList_success() {
+        Index indexLastPerson = Index.fromOneBased(model.getFilteredPersonList().size());
+        Person lastPerson = model.getFilteredPersonList().get(indexLastPerson.getZeroBased());
+
+        PersonBuilder personInList = new PersonBuilder(lastPerson);
+        Person editedPerson = personInList.withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
+                .build();
+
+        UpdatePersonDescriptor descriptor = new UpdatePersonDescriptorBuilder().withNric(VALID_NRIC_AMY)
+                .withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY).withAddress(VALID_ADDRESS_AMY)
+                .withDateOfBirth(VALID_DATEOFBIRTH_AMY).withSex(VALID_SEX_AMY).withStatus(VALID_STATUS_AMY).build();
+        UpdateCommand updateCommand = new UpdateCommand(descriptor.getNric(), descriptor);
+
+        String expectedMessage = String.format(
+                UpdateCommand.MESSAGE_UPDATE_PERSON_SUCCESS, Messages.format(editedPerson));
+
+        Model expectedModel = new ModelManager(new ImmuniMate(model.getImmuniMate()), new UserPrefs());
+        expectedModel.setPerson(lastPerson, editedPerson);
+
+        assertCommandSuccess(updateCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_noFieldSpecifiedUnfilteredList_success() {
+        UpdateCommand updateCommand = new UpdateCommand(INDEX_FIRST_PERSON, new UpdatePersonDescriptor());
+        Person editedPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+
+        String expectedMessage = String.format(
+                UpdateCommand.MESSAGE_UPDATE_PERSON_SUCCESS, Messages.format(editedPerson));
+
+        Model expectedModel = new ModelManager(new ImmuniMate(model.getImmuniMate()), new UserPrefs());
+
+        assertCommandSuccess(updateCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_filteredList_success() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        Person personInFilteredList = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(personInFilteredList).withName(VALID_NAME_BOB).build();
+        UpdateCommand updateCommand = new UpdateCommand(INDEX_FIRST_PERSON,
+                new UpdatePersonDescriptorBuilder().withName(VALID_NAME_BOB).build());
+
+        String expectedMessage = String.format(
+                UpdateCommand.MESSAGE_UPDATE_PERSON_SUCCESS, Messages.format(editedPerson));
+
+        Model expectedModel = new ModelManager(new ImmuniMate(model.getImmuniMate()), new UserPrefs());
+        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
+
+        assertCommandSuccess(updateCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_duplicatePersonUnfilteredList_failure() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        UpdatePersonDescriptor descriptor = new UpdatePersonDescriptorBuilder(firstPerson).build();
+        UpdateCommand updateCommand = new UpdateCommand(INDEX_SECOND_PERSON, descriptor);
+
+        assertCommandFailure(updateCommand, model, UpdateCommand.MESSAGE_DUPLICATE_PERSON);
+    }
+
+    @Test
+    public void execute_duplicatePersonFilteredList_failure() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        // edit person in filtered list into a duplicate in address book
+        Person personInList = model.getImmuniMate().getPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
+        UpdateCommand updateCommand = new UpdateCommand(INDEX_FIRST_PERSON,
+                new UpdatePersonDescriptorBuilder(personInList).build());
+
+        assertCommandFailure(updateCommand, model, UpdateCommand.MESSAGE_DUPLICATE_PERSON);
+    }
+
+    @Test
+    public void execute_invalidPersonIndexUnfilteredList_failure() {
+        // Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        UpdatePersonDescriptor descriptor = new UpdatePersonDescriptorBuilder().withName(VALID_NAME_BOB).build();
+        UpdateCommand updateCommand = new UpdateCommand(new Nric("S1234567X"), descriptor);
+        assertCommandFailure(updateCommand, model, Messages.MESSAGE_PERSON_NOT_FOUND);
+    }
+
+    /**
+     * Edit filtered list where index is larger than size of filtered list,
+     * but smaller than size of address book
+     */
+    @Test
+    public void execute_invalidPersonNricFilteredList_failure() {
+        // showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        // Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        // assertTrue(outOfBoundIndex.getZeroBased() < model.getImmuniMate().getPersonList().size());
+
+        UpdateCommand updateCommand = new UpdateCommand(new Nric("S1234567X"),
+                new UpdatePersonDescriptorBuilder().withName(VALID_NAME_BOB).build());
+
+        assertCommandFailure(updateCommand, model, Messages.MESSAGE_PERSON_NOT_FOUND);
+    }
+
+    @Test
+    public void equals() {
+        final UpdateCommand standardCommand = new UpdateCommand(new Nric(VALID_NRIC_AMY), DESC_AMY);
+
+        // same values -> returns true
+        UpdatePersonDescriptor copyDescriptor = new UpdatePersonDescriptor(DESC_AMY);
+        UpdateCommand commandWithSameValues = new UpdateCommand(new Nric(VALID_NRIC_AMY), copyDescriptor);
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different index -> returns false
+        assertFalse(standardCommand.equals(new UpdateCommand(new Nric(VALID_NRIC_BOB), DESC_AMY)));
+
+        // different descriptor -> returns false
+        assertFalse(standardCommand.equals(new UpdateCommand(new Nric(VALID_NRIC_AMY), DESC_BOB)));
+    }
+
+    @Test
+    public void toStringMethod() {
+        UpdatePersonDescriptor descriptor = new UpdatePersonDescriptorBuilder().withNric(VALID_NRIC_AMY)
+                .withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY).withAddress(VALID_ADDRESS_AMY)
+                .withDateOfBirth(VALID_DATEOFBIRTH_AMY).withSex(VALID_SEX_AMY).withStatus(VALID_STATUS_AMY).build();
+        UpdateCommand updateCommand = new UpdateCommand(new Nric(VALID_NRIC_AMY), descriptor);
+        String expected = UpdateCommand.class.getCanonicalName()
+                + "{nric=" + VALID_NRIC_AMY
+                + ", name=" + VALID_NAME_AMY
+                + ", phone=" + VALID_PHONE_AMY
+                + ", address=" + VALID_ADDRESS_AMY
+                + ", date of birth=" + VALID_DATEOFBIRTH_AMY
+                + ", sex=" + VALID_SEX_AMY
+                + ", status=" + VALID_STATUS_AMY
+                + "}";
+        assertEquals(expected, updateCommand.toString());
+    }
+}
