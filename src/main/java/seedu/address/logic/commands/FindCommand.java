b06a70fcee47e24defb11e2bc5e849c160bfa17d
NatLeong <>
2024-04-04 03:48:10 +0800

    Fix find command result

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -1,6 +1,9 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONDITION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import java.util.function.Predicate;
 
@@ -17,10 +20,13 @@ public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names, addresses or conditions"
+            + " contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Example: " + COMMAND_WORD + " " + PREFIX_NAME + "alice bob charlie" + ", "
+            + COMMAND_WORD + " " + PREFIX_ADDRESS + "Serangoon" + ", "
+            + COMMAND_WORD + " " + PREFIX_CONDITION + "Diabetes";
 
     private final Predicate<Person> predicate;
     //TODO: add nric contains keywords
